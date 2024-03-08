cdbbc0f0a5ac5f25197dc6f609ca2b9d4ce91843
LopStevn <stevenlpz03@gmail.com>
2024-03-08 10:46:52 -0600

    Agregando Servicios - Controlador de Productos con primera vista

---
+++ Ecommerce/Controllers/ProductoController.cs
@@ -0,0 +1,28 @@
+﻿using Ecommerce.Models.DATA;
+using Ecommerce.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly EcommerceDbContext _context;
+        private readonly IServicioImagen _servicioImagen;
+        private readonly IServicioLista _servicioLista;
+
+        public ProductoController(EcommerceDbContext context, IServicioImagen servicioImagen, IServicioLista servicioLista)
+        {
+            _context = context;
+            _servicioImagen = servicioImagen;
+            _servicioLista = servicioLista;
+        }
+
+        public async Task<IActionResult> Lista()
+        {
+            return View(await _context.Productos
+                .Include(p => p.Categoria)
+                .ToListAsync());
+        }
+    }
+}
---
+++ Ecommerce/Services/ServicioLista.cs
@@ -0,0 +1,35 @@
+﻿using Ecommerce.Models.DATA;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Services
+{
+    public class ServicioLista : IServicioLista
+    {
+        private readonly EcommerceDbContext _context;
+
+        public ServicioLista(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetListaCategorias()
+        {
+            List<SelectListItem> lista = await _context.Categorias.Select(x => new SelectListItem
+            {
+                Text = x.Nombre,
+                Value = $"{x.Id}"
+            })
+                .OrderBy(x => x.Text)
+                .ToListAsync();
+
+            lista.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione una categoria]",
+                Value = "0"
+            });
+
+            return lista;
+        }
+    }
+}
